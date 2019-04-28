cbc37ba6142efae270bcc7c3f9a03554c522ae3c
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-04-28 19:06:55 -0400

    trying to incorporate doctest

---
+++ project2.2_files/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define DOCTEST_CONFIG_IMLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "hashtable.h"
+#include "subway_portal.h"
+#include "subway_route.h"
+#include "subway_station.h"
+#include "subway_system.h"
+
+TEST_CASE("Testing Project 2") {}
